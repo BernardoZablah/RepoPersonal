b162e41271e56c3fc678add6a72949184923afa4
Bernie Zablah <bernardo.zablah@alumnos.uneatlantico.es>
2024-11-04 17:31:42 +0100

    Dificultad facil funcional

--- src/ExamenAutomaticoPersonalizado.java
+++ src/ExamenAutomaticoPersonalizado.java
@@ -7,39 +7,84 @@ public class ExamenAutomaticoPersonalizado {
         final String CONFIGURACION = "[1] Configuracion";
         final String EJECUCUCION_DEL_TEST = "[2] Ejecucion del test";
         final String SALIR_DEL_SISTEMA = "[3] Salir del sistema";
-        
-        int seleccionUsuario =0;
+        final String STRING_DIFICULTAD_FACIL = "[1] Dificultad Facil";
+        final String STRING_DIFICULTAD_MEDIA = "[2] Dificultad Media";
+        final String STRING_DIFICULTAD_ALTA = "[3] Dificultad Alta";
+        int seleccionUsuarioMenuPrincipal = 0;
+        int seleccionPrimerFactor = 0;
+        int seleccionNivelDificultad = 0;
+        final int LIMITE_INFERIOR_DIFICULTAD_FACIL = 1;
+        final int LIMITE_SPUERIOR_DIFICULTAD_FACIL = 6;
+        final int LIMITE_INFERIOR_DIFICULTAD_MEDIA = 1;
+        final int LIMITE_SUPERIOR_DIFICULTAD_MEDIA = 10;
+        final int LIMITE_INFERIOR_DIFICULTAD_ALTA = 1;
+        final int LIMITE_SUPERIOR_DIFICULTAD_ALTA = 20;
+        final int DIFICULTAD_FACIL = 1;
+        final int DIFICULTAD_MEDIA = 2;
+        final int DIFICULTAD_ALTA = 3;
+        int numeroPreguntas = 0;
+        int variableMultiplicacion;
+        int respuestaUsuario;
+        double contadorPuntos = 0;
 
-        do{
-        System.out.println(CONFIGURACION);
-        System.out.println(EJECUCUCION_DEL_TEST);
-        System.out.println(SALIR_DEL_SISTEMA);
+        do {
+            System.out.println(CONFIGURACION);
+            System.out.println(EJECUCUCION_DEL_TEST);
+            System.out.println(SALIR_DEL_SISTEMA);
+            seleccionUsuarioMenuPrincipal = scanner.nextInt();
 
-        seleccionUsuario = scanner.nextInt();
+            if (seleccionUsuarioMenuPrincipal == 1) {
+                System.out.println("Elije de que numero quieres estudiar las tablas");
+                seleccionPrimerFactor = scanner.nextInt();
+                System.out.println("Elije el nivel de dificultad deseado:");
+                System.out.println(STRING_DIFICULTAD_FACIL);
+                System.out.println(STRING_DIFICULTAD_MEDIA);
+                System.out.println(STRING_DIFICULTAD_ALTA);
+                seleccionNivelDificultad = scanner.nextInt();
 
-        if (seleccionUsuario==1){
+                if (seleccionNivelDificultad == 1) {
+                    System.out.println("Has elejido la dificultad facil");
+                    seleccionNivelDificultad = DIFICULTAD_FACIL;
+                } else if (seleccionNivelDificultad == 2) {
+                    System.out.println("Has elejido la dificultad media");
+                    seleccionNivelDificultad = DIFICULTAD_MEDIA;
+                } else if (seleccionNivelDificultad == 3) {
+                    System.out.println("Has elejido la dificultad alta");
+                    seleccionNivelDificultad = DIFICULTAD_ALTA;
+                } else {
+                    System.out.println("Selecciona una opcion valida");
+                }
+
+                System.out.println("Configuracion aplicada correctamente.");
 
 
+            } else if (seleccionUsuarioMenuPrincipal == 2) {
+                if (seleccionNivelDificultad == DIFICULTAD_FACIL) {
+                    for(numeroPreguntas = 1; numeroPreguntas <= 5; numeroPreguntas++ ){
+                        variableMultiplicacion = (int) (Math.random() * LIMITE_SPUERIOR_DIFICULTAD_FACIL - LIMITE_INFERIOR_DIFICULTAD_FACIL + 1) + LIMITE_INFERIOR_DIFICULTAD_FACIL;
+                        System.out.println(seleccionPrimerFactor + "x" + variableMultiplicacion);
+                        respuestaUsuario = scanner.nextInt();
+                        if (respuestaUsuario == (seleccionPrimerFactor*variableMultiplicacion)) {
+                            contadorPuntos = contadorPuntos + 2;
+                        } else {
+                            contadorPuntos = contadorPuntos - 0.5;
+                        }
+                        System.out.println("Has obtenido " + contadorPuntos + " puntos");
+                    }
+                } else if (seleccionNivelDificultad == DIFICULTAD_MEDIA) {
 
+                } else if (seleccionNivelDificultad == DIFICULTAD_ALTA) {
 
+                }
 
+            } else if (seleccionUsuarioMenuPrincipal == 3) {
+                System.out.println("Saliendo del programa...");
+            } else {
+                System.out.println("Selecciona una opcion valida");
+            }
 
-        } else if (seleccionUsuario == 2){
+        } while (seleccionUsuarioMenuPrincipal != 3);
 
-
-
-
-
-
-
-        } else if (seleccionUsuario == 3){
-            System.out.println("Saliendo del programa...");
-        } else {
-            System.out.println("Seleccione una opcion valida");
-        }
-
-        } while( seleccionUsuario != 3);
-    
         scanner.close();
     }
 }
