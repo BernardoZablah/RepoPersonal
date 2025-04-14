9ade283ab49c35da1744ed075a30ad5a94d79db9
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-04-15 01:05:36 +0200

    Teoria + Launcher Flipado

---
+++ src/ModelacionFuncion/Launcher.java
@@ -0,0 +1,10 @@
+package ModelacionFuncion;
+
+import Cajero.MainMenu;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.start();
+    }
+}
