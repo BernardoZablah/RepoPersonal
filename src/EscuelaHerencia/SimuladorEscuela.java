57e25c0076b2df98fb60de559785143d84560e11
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-06-08 01:08:13 +0200

    Primera clase SimuladorEscuela

---
+++ src/EscuelaHerencia/SimuladorEscuela.java
@@ -0,0 +1,10 @@
+package EscuelaHerencia;
+
+import ModelacionFuncion.MainMenu;
+
+public class SimuladorEscuela {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.start();
+    }
+}
