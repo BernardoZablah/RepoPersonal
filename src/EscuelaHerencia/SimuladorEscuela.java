1f87142fbd4b3c685524e968ddaff86803e7df4f
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-06-08 01:40:59 +0200

    primera practica herencia terminada

--- src/EscuelaHerencia/SimuladorEscuela.java
+++ src/EscuelaHerencia/SimuladorEscuela.java
@@ -1,10 +1,11 @@
 package EscuelaHerencia;
 
-import ModelacionFuncion.MainMenu;
-
 public class SimuladorEscuela {
     public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.start();
+        Persona estudiante = new Estudiante("Ana", 17, "2º Bachillerato");
+        Persona profesor = new Profesor("Carlos", 45, "Matemáticas");
+
+        estudiante.presentarse();
+        profesor.presentarse();
     }
 }
